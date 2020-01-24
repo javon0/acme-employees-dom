23fb3894ad76fda6833436118453747fcaf8abca
Josh Avon <joshuacalebavon@gmail.com>
2020-01-23 20:15:23 -0500

    phase one

---
+++ app.js
@@ -0,0 +1,22 @@
+const employees = [
+    { id: 1, name: 'moe'},
+    { id: 2, name: 'larry'},
+    { id: 4, name: 'shep'},
+    { id: 3, name: 'curly'},
+    { id: 5, name: 'groucho'},
+    { id: 6, name: 'harpo'},
+    { id: 8, name: 'shep Jr.'},
+    { id: 99, name: 'lucy'}
+  ];
+
+  const list = document.querySelector('div')
+
+  const render = () => {
+      const html = employees.map(employee => {
+          return `<div class = 'employee'>${employee.name}</div>`
+      }).join('')
+
+      list.innerHTML = html
+  }
+
+  render()
